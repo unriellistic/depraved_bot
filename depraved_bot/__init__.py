9e5b59e5e3c53bb631ac817defc4624c9ff4d2da
unriellistic <charliewindle@gmail.com>
2023-02-19 16:33:31 +0800

    added the skeleton for a button command

--- depraved_bot/__init__.py
+++ depraved_bot/__init__.py
@@ -1,5 +1,7 @@
+import os
 import disnake
 from disnake.ext import commands
+from dotenv import load_dotenv
 
 from depraved_bot.config import *
 
@@ -8,18 +10,42 @@ command_sync_flags.sync_commands_debug = True
 
 intents = disnake.Intents.default()
 
-bot = commands.Bot(
+bot = commands.InteractionBot(
     command_sync_flags=command_sync_flags,
     intents=intents,
 )
 
 @bot.slash_command(description="Ping me to check if I'm alive.")
-async def ping(interaction):
-    await interaction.response.send_message("Hello! I'm alive!")
+async def ping(inter):
+    await inter.response.send_message("Hello! I'm alive!")
+
+
+class RoleCheckButton(disnake.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @disnake.ui.button(label="I've selected all my kinks")
+    async def check_roles(self, button, inter):
+        # do role checking here
+        if True:
+            await inter.response.send_message("Welcome to the server!", ephemeral = True)
+
+@bot.slash_command(description="Set up the role checking button in this channel.")
+async def setup_button(inter):
+    view = RoleCheckButton()
+    await inter.response.send_message("Please click this button when you're done selecting all your kink roles.", view=view)
+
 
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}! (ID: {bot.user.id})\n-------")
 
 if __name__ == "__main__":
-    bot.run(DISCORD_TOKEN_ALPHA) # change to DISCORD_TOKEN for production
+    load_dotenv()
+    env = os.getenv("ENVIRONMENT", "dev")
+    if env in ["dev", "development"]:
+        bot.run(os.getenv("DISCORD_TOKEN_DEV"))
+    elif env in ["prod", "production"]:
+        bot.run(os.getenv("DISCORD_TOKEN_PROD"))
+    else:
+        print("invalid ENVIRONMENT, exiting.")
