c8343ddb5c3481a5a9d45318cb22938c0c46c070
unriellistic <charliewindle@gmail.com>
2023-06-03 09:42:54 +0800

    moved to raw reaction events, hopefully solving reaction add/remove failures

--- depraved_bot/cogs/role_checker.py
+++ depraved_bot/cogs/role_checker.py
@@ -54,20 +54,31 @@ class RoleCheckerCog(commands.Cog):
                 break
 
     @commands.Cog.listener()
-    async def on_reaction_add(self, reaction: disnake.Reaction, member: disnake.Member):
-        # if the reaction was added by this bot
-        if member.id == self.bot.user.id:
-            return
+    async def on_raw_reaction_add(self, payload: disnake.RawReactionActionEvent):
+        rxn_name = payload.emoji.name
+        msg_id = payload.message_id
+        ch_id = payload.channel_id
+        user_id = payload.user_id
+        guild_id = payload.guild_id
 
+        channel = self.bot.get_channel(ch_id)
+        msg = await channel.fetch_message(msg_id)
+        guild = self.bot.get_guild(guild_id)
+        member = guild.get_member(user_id)
+
+        # if the reaction was added by this bot
+        if user_id == self.bot.user.id:
+            return
+        
         # if the message is not from this bot
-        if reaction.message.author.id != self.bot.user.id:
+        if msg.author.id != self.bot.user.id:
             return
         
         # message has no embeds (not the right message)
-        if not reaction.message.embeds:
+        if not msg.embeds:
             return
         
-        kink_name = reaction.message.embeds[0].title
+        kink_name = msg.embeds[0].title
         kink_to_add = next((x for x in self.optional_kinks if x.name == kink_name), None)
 
         # kink name isn't in the list for some reason (reaction done on wrong embed)
@@ -75,50 +86,130 @@ class RoleCheckerCog(commands.Cog):
             return
         
         # remove other reactions on the message (this will also trigger reaction remove event and remove associated roles)
-        for existing_reaction in reaction.message.reactions:
+        for existing_reaction in msg.reactions:
             # don't remove the reaction they just added
-            if existing_reaction == reaction:
+            if existing_reaction.emoji == payload.emoji.name:
                 continue
             rxn_users = await existing_reaction.users().flatten()
-            if member.id in (user.id for user in rxn_users):
-                await reaction.message.remove_reaction(existing_reaction, member)
-
-
-        green_role = member.guild.get_role(kink_to_add.green)
-        yellow_role = member.guild.get_role(kink_to_add.yellow)
-        red_role = member.guild.get_role(kink_to_add.red)
+            if user_id in (user.id for user in rxn_users):
+                await msg.remove_reaction(existing_reaction, member)
+                
+        green_role = guild.get_role(kink_to_add.green)
+        yellow_role = guild.get_role(kink_to_add.yellow)
+        red_role = guild.get_role(kink_to_add.red)
 
         # actually add the role now, based on the emoji
-        if reaction.emoji == "🟩":
+        if rxn_name == "🟩":
             await member.add_roles(green_role)
-        elif reaction.emoji == "🟨":
+        elif rxn_name == "🟨":
             await member.add_roles(yellow_role)
-        elif reaction.emoji == "🟥":
-            await member.add_roles(red_role)
+        elif rxn_name == "🟥":
+            await member.add_roles(red_role)        
 
+    # @commands.Cog.listener()
+    # async def on_reaction_add(self, reaction: disnake.Reaction, member: disnake.Member):
+    #     # if the reaction was added by this bot
+    #     if member.id == self.bot.user.id:
+    #         return
+
+    #     # if the message is not from this bot
+    #     if reaction.message.author.id != self.bot.user.id:
+    #         return
+        
+    #     # message has no embeds (not the right message)
+    #     if not reaction.message.embeds:
+    #         return
+        
+    #     kink_name = reaction.message.embeds[0].title
+    #     kink_to_add = next((x for x in self.optional_kinks if x.name == kink_name), None)
+
+    #     # kink name isn't in the list for some reason (reaction done on wrong embed)
+    #     if not kink_to_add:
+    #         return
+        
+    #     # remove other reactions on the message (this will also trigger reaction remove event and remove associated roles)
+    #     for existing_reaction in reaction.message.reactions:
+    #         # don't remove the reaction they just added
+    #         if existing_reaction == reaction:
+    #             continue
+    #         rxn_users = await existing_reaction.users().flatten()
+    #         if member.id in (user.id for user in rxn_users):
+    #             await reaction.message.remove_reaction(existing_reaction, member)
+
+
+    #     green_role = member.guild.get_role(kink_to_add.green)
+    #     yellow_role = member.guild.get_role(kink_to_add.yellow)
+    #     red_role = member.guild.get_role(kink_to_add.red)
+
+    #     # actually add the role now, based on the emoji
+    #     if reaction.emoji == "🟩":
+    #         await member.add_roles(green_role)
+    #     elif reaction.emoji == "🟨":
+    #         await member.add_roles(yellow_role)
+    #     elif reaction.emoji == "🟥":
+    #         await member.add_roles(red_role)
+
+    # @commands.Cog.listener()
+    # async def on_reaction_remove(self, reaction: disnake.Reaction, member: disnake.Member):
+    #     # if the message is not from this bot
+    #     if reaction.message.author.id != self.bot.user.id:
+    #         return
+        
+    #     # message has no embeds (not the right message)
+    #     if not reaction.message.embeds:
+    #         return
+        
+    #     kink_name = reaction.message.embeds[0].title
+    #     kink_to_add = next((x for x in self.optional_kinks if x.name == kink_name), None)
+
+    #     # kink name isn't in the list for some reason (likely reaction done on wrong embed)
+    #     if not kink_to_add:
+    #         return
+        
+    #     # figure out which role to remove, based on the emoji
+    #     if reaction.emoji == "🟩":
+    #         role = kink_to_add.green
+    #     elif reaction.emoji == "🟨":
+    #         role = kink_to_add.yellow
+    #     elif reaction.emoji == "🟥":
+    #         role = kink_to_add.red
+
+    #     await member.remove_roles(member.guild.get_role(role))
+    
     @commands.Cog.listener()
-    async def on_reaction_remove(self, reaction: disnake.Reaction, member: disnake.Member):
+    async def on_raw_reaction_remove(self, payload: disnake.RawReactionActionEvent):
+        rxn_name = payload.emoji.name
+        msg_id = payload.message_id
+        ch_id = payload.channel_id
+        user_id = payload.user_id
+        guild_id = payload.guild_id
+
+        channel = self.bot.get_channel(ch_id)
+        msg = await channel.fetch_message(msg_id)
+        guild = self.bot.get_guild(guild_id)
+        member = guild.get_member(user_id)
+
         # if the message is not from this bot
-        if reaction.message.author.id != self.bot.user.id:
+        if msg.author.id != self.bot.user.id:
             return
         
         # message has no embeds (not the right message)
-        if not reaction.message.embeds:
+        if not msg.embeds:
             return
         
-        kink_name = reaction.message.embeds[0].title
+        kink_name = msg.embeds[0].title
         kink_to_add = next((x for x in self.optional_kinks if x.name == kink_name), None)
 
-        # kink name isn't in the list for some reason (likely reaction done on wrong embed)
+        # kink name isn't in the list for some reason (reaction done on wrong embed)
         if not kink_to_add:
             return
-        
+
         # figure out which role to remove, based on the emoji
-        if reaction.emoji == "🟩":
+        if rxn_name == "🟩":
             role = kink_to_add.green
-        elif reaction.emoji == "🟨":
+        elif rxn_name == "🟨":
             role = kink_to_add.yellow
-        elif reaction.emoji == "🟥":
+        elif rxn_name == "🟥":
             role = kink_to_add.red
 
         await member.remove_roles(member.guild.get_role(role))
